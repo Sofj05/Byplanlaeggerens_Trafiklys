d947a7b051b22f0d1ed634f690f6b1a200effd24
Sofj05 <soj011@edu.zealand.dk>
2025-09-08 12:15:52 +0200

    hgj

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,65 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Hvad tid på døgnet er det? (Dag, Aften, Nat)");
+        String time = input.nextLine();
+
+        System.out.println("Er trafikmængden høj eller lav?");
+        String trafficMass = input.nextLine();
+
+        System.out.println("I hvilket område kører du? (Bolig, Erhverv, Blandet)");
+        String area = input.nextLine();
+
+        System.out.println("Er der en nødsituation? (True/False)");
+        boolean emergency = input.nextBoolean();
+
+        String light = "";
+        String access = "";
+
+// Første: bestemme lysfarve
+        if (emergency) {
+            light = "Grønt lys (Nødsituation)";
+        } else {
+            switch (time) {
+                case "Dag":
+                    if (trafficMass.equals("Lav")) {
+                        light = "Grønt Lys";
+                    } else {
+                        light = "Rødt lys"; // Høj trafik om dagen
+                    }
+                    break;
+
+                case "Aften":
+                    light = "Gult lys";
+                    break;
+
+                case "Nat":
+                    if (trafficMass.equals("Høj")) {
+                        light = "Rødt lys";
+                    } else {
+                        light = "Gult lys"; //nat med lav trafik
+                    }
+                    break;
+                default:
+                    light = "Ugyldigt tidspunkt";
+            }
+        }
+
+        // Dernæst: bestemme adgang
+        if (area.equals("Erhverv") || (area.equals("Blandet") && trafficMass.equals("Lav"))) {
+            access = "Adgang tilladt";
+        } else if (area.equals("Bolig") && time.equals("Nat")) {
+                access = "Adgang forbudt (Boligområde om natten)";
+            } else {
+                access = "Adgang begrænset til beboere og gæster i dagstimerne";
+        }
+
+            System.out.println("Trafiklys status: " + light);
+            System.out.println("Adgangs status: " + access);
+
+
         }
     }
-}
